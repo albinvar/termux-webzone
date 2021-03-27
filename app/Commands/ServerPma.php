e9fc1e5a5c4bc0b7d7c129e026939113e1f4b3de
Albin Varghese <albin8285@gmail.com>
2021-03-27 17:16:27 +0530

    create command to start php my admin

--- app/Commands/ServerPma.php
+++ app/Commands/ServerPma.php
@@ -5,7 +5,7 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
-class Server extends Command
+class ServerPma extends Command
 {
     /**
      * The signature of the command.
@@ -32,9 +32,23 @@ class Server extends Command
     }
     
     public function checkInstallations()
-    {
-    	
+	    {$this->info("");
+    	$this->logo();
+	    $this->info("\n");
+    	$port = 8977;
+	    $dir = config('pma.PMA_DIR');
+    	$cmd = "php -S 127.0.0.1:{$port} -t {$dir}/pma";
+		$this->comment("Starting phpmyadmin web interface at : http://127.0.0.1:{$port}");
+		$this->info("");
+		shell_exec("xdg-open http://127.0.0.1:{$port}");
+	    shell_exec($cmd);
     }
+    
+    public function logo()
+	{
+		 $figlet = new \Laminas\Text\Figlet\Figlet();
+		echo $figlet->setFont(config('logo.font'))->render(config('logo.name'));
+	}
     
     /**
      * Define the command's schedule.
