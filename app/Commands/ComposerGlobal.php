43dab6f4489a628ca9cc061139f6524c6501d372
Albin Varghese <albin8285@gmail.com>
2021-03-28 11:17:20 +0530

    check if composer is installed or not

--- app/Commands/ComposerGlobal.php
+++ app/Commands/ComposerGlobal.php
@@ -12,14 +12,14 @@ class ComposerGlobal extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'composer:global';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Make composer commands global';
 
     /**
      * Execute the console command.
@@ -28,9 +28,32 @@ class ComposerGlobal extends Command
      */
     public function handle()
     {
-        //
+    	$this->composer = "/data/data/com.termux/files/usr/bin/composer";
+        $this->checkInstallation();
     }
-
+    
+    private function checkInstallation()
+    {
+    	$this->logo();
+	    $this->info("\n");
+	    $is_installed = $this->task("Check whether composer is installed ", function () {
+     	
+            if(file_exists($this->composer))
+            { return true; }
+            else
+			{ return false; }
+        });
+        
+        
+        
+    }
+    
+    public function logo()
+	{
+		 $figlet = new \Laminas\Text\Figlet\Figlet();
+		echo $figlet->setFont(config('logo.font'))->render(config('logo.name'));
+	}
+    
     /**
      * Define the command's schedule.
      *
