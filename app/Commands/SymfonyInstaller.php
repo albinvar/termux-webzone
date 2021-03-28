8353b79a30c9075cb7f20627c2ce0835e9501b00
Albin Varghese <albin8285@gmail.com>
2021-03-28 18:59:07 +0530

    add symfony installer (not working)

--- app/Commands/SymfonyInstaller.php
+++ app/Commands/SymfonyInstaller.php
@@ -28,7 +28,46 @@ class SymfonyInstaller extends Command
      */
     public function handle()
     {
-        //
+    	$this->dir = "/data/data/com.termux/files/usr/bin";
+        $this->checkInstallation();
+    }
+    
+    public function checkInstallation()
+    {
+    	$link = "https://get.symfony.com/cli/installer";
+		$lines = shell_exec("curl -w '\n%{http_code}\n' {$link} -o {$this->dir}/symfony && chmod +x {$this->dir}/symfony");
+	    $lines = explode("\n", trim($lines));
+		$status = $lines[count($lines)-1];
+		$this->checkDownloadStatus($status, $this->dir);
+    }
+    
+    private function checkDownloadStatus(Int $status, $dir)
+    {
+    	switch ($status) {
+  case 000:
+    $this->error("Cannot connect to Server");
+    break;
+  case 200:
+    $this->comment("\nDownloaded Successfully...!!!");
+    $this->runTasks();
+    break;
+  case 404:
+    $this->error("File not found on server..");
+    break;
+  default:
+    $this->error("An Unknown Error occurred...");
+}
+    }
+    
+    private function runTasks()
+    {
+    	$this->task("verifying command ", function () {
+     	
+            if(file_exists($this->dir.'/symfony'))
+            { return true; }
+            else
+			{ return false; }
+        });
     }
 
     /**
