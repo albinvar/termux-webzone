7776413959c9231b7b0d2a187a81d6cb442db972
Albin Varghese <albin8285@gmail.com>
2021-03-28 06:29:57 +0530

    create development server

---
+++ app/Commands/DevelopmentServer.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class DevelopmentServer extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:dev
+							{--port=8088}
+							{--n}';
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Start localhost for development';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->checkInstallation();
+    }
+    
+    public function checkInstallation()
+    {
+    	$path = "/sdcard/www";
+    
+    	if(!is_dir($path)){
+	    	mkdir($path);
+			$this->comment('Directory created..');
+	    }
+	 $this->start();
+    }
+    
+    private function start()
+    {
+    	$path = "/sdcard/www";
+    	$this->logo();
+	    $this->comment("Localhost Services Started....");
+	    $this->line("\n");
+		$this->launch();
+    	$cmd = exec("php -S 127.0.0.1:{$this->option('port')} -t {$path}");
+    }
+    
+    private function launch()
+    {
+    	if(!$this->option('n'))
+	    {
+    	return shell_exec("xdg-open http://127.0.0.1:{$this->option('port')}");
+	    }
+    }
+    
+    public function logo()
+	{
+		 $figlet = new \Laminas\Text\Figlet\Figlet();
+		echo $figlet->setFont(config('logo.font'))->render(config('logo.name'));
+	}
+    
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
