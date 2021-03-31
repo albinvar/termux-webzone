267b78d489726db0f0057163ff4e36ade25a89b5
Albin Varghese <albin8285@gmail.com>
2021-03-31 20:11:16 +0530

    specify location for storing app prefernces

--- config/filesystem.php
+++ config/filesystem.php
@@ -5,7 +5,7 @@ return [
     'disks' => [
         'local' => [
             'driver' => 'local',
-            'root' => getcwd(),
+            'root' => "/data/data/com.termux/files/usr/var/webzone",
         ],
     ],
 ];
