46be35dce189a199cdb599a620528dd3f2d25fda
Albin Varghese <albin8285@gmail.com>
2021-04-01 16:33:14 +0530

    create basic josn file uodater

--- app/Commands/Settings.php
+++ app/Commands/Settings.php
@@ -43,14 +43,13 @@ class Settings extends Command
     {
     	echo exec('clear');
     	$this->logo();
-	    $this->newLine();
     	$this->showList();
     }
     
     public function getOptions()
     {
     	$json_object = file_get_contents(config('settings.PATH').'/settings.json');
-		$data = json_decode($json_object, false);
+		$data = json_decode($json_object, true);
     	return $data;
     }
     
@@ -66,8 +65,60 @@ class Settings extends Command
         'What would you like to modify',
         [1 => 'Project Root', 'Localhost Port', 'MySql Port', 'Ngrok port', 'PhpMyAdmin Port', 9 => 'Exit']
     );
-
-    $this->info("Source chosen is $source");
+	    switch($source)
+		{
+			case 'Project Root':
+				$body = "Project Root";
+				$key = 'project_dir';
+				$path = $this->dirUpdater();
+				if(!$this->checkDir($path)){
+					$this->error('Invalid path');
+					sleep(4);
+					exec('clear');
+					return $this->call('settings');
+				}
+				$type = "normal";
+				$this->edit($body, $key, $path, $type);
+				break;
+			case 'Exit':
+				$this->info('Thanks for using me');
+				break;
+		}
+    }
+    
+    private function edit($description, $key, $default, $type)
+    {
+    	$data = $this->getOptions();
+	    $data[$key] = $default;
+		$this->update($data);
+    
+    }
+    
+    private function update($data)
+    {
+    	$json_object = json_encode($data);
+		file_put_contents(config('settings.PATH').'/settings.json', $json_object);
+    	$this->info('updated');
+    }
+    
+    private function checkDir($path)
+    {
+    	if(is_dir($path))
+	    {
+			return true;
+		} else {
+			return false;
+		}
+    }
+    
+    private function dirUpdater($q="Enter folder path")
+    {
+    	echo exec('clear');
+	    $this->logo();
+		$this->newLine();
+    	$path = $this->ask($q);
+		return $path;
+			
     }
     
     /**
