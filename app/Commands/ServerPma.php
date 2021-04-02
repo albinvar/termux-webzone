8bb2bf67cbcf1be9d7f2e8b8436e23d594a9f838
Albin Varghese <albin8285@gmail.com>
2021-04-02 12:26:38 +0530

    updadte pma port based on users settings

--- app/Commands/ServerPma.php
+++ app/Commands/ServerPma.php
@@ -14,7 +14,7 @@ class ServerPma extends Command
      */
     protected $signature = 'server:pma
 							{--n}
-							{--port=8999}';
+							{--port=}';
 
     /**
      * The description of the command.
@@ -30,6 +30,7 @@ class ServerPma extends Command
      */
     public function handle()
     {
+    	$this->setPort();
         $this->checkInstallations();
     }
     
@@ -39,10 +40,9 @@ class ServerPma extends Command
 	    $this->info("");
     	$this->logo();
 	    $this->info("\n");
-    	$port = $this->option('port');
 	    $dir = config('pma.PMA_DIR');
-    	$cmd = "php -S 127.0.0.1:{$port} -t {$dir}/pma";
-		$this->comment("Starting phpmyadmin web interface at : http://127.0.0.1:{$port}");
+    	$cmd = "php -S 127.0.0.1:{$this->port} -t {$dir}/pma";
+		$this->comment("Starting phpmyadmin web interface at : http://127.0.0.1:{$this->port}");
 		$this->info("");
 		$this->launch();
 	    shell_exec($cmd);
@@ -53,13 +53,31 @@ class ServerPma extends Command
 		 $figlet = new \Laminas\Text\Figlet\Figlet();
 		 echo $figlet->setFont(config('logo.font'))->render(config('logo.name'));
 	}
+	
+	private function setPort()
+    {
+    	if(!empty($this->option('port'))){
+		    $this->port = $this->option('port');
+    	} elseif(!empty($this->getPort())){
+	    	$this->port = $this->getPort();
+		} else {
+			$this->port = config('pma.PMA_PORT');
+		}
+    }
     
     private function launch()
     {
     	if(!$this->option('n'))
 	    {
-    	return shell_exec("xdg-open http://127.0.0.1:{$this->option('port')}");
+    	return shell_exec("xdg-open http://127.0.0.1:{$this->port}");
 	    }
+    }
+    
+    public function getPort()
+    {
+    	$json_object = file_get_contents(config('settings.PATH').'/settings.json');
+		$data = json_decode($json_object, true);
+    	return $data['pma_port'];
     }
     
     /**
