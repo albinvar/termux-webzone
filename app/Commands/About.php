f99656a96bf9b870b9cd2cd7456ce68b43665c60
Albin Varghese <albin8285@gmail.com>
2021-04-21 17:16:23 +0530

    fix typo

--- app/Commands/About.php
+++ app/Commands/About.php
@@ -19,7 +19,7 @@ class About extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'About Webzone';
 
     /**
      * Execute the console command.
