3c7b6d4bb3c49f3a3cc92306640880ddeb53d98f
Albin Varghese <albin8285@gmail.com>
2021-06-06 00:59:00 +0530

    create nette command

---
+++ app/Commands/Create/Nette.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Commands\Create;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class Nette extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:nette
+							{name?}
+							{--path=}';
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Create Nette projects';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->callSilently('settings:init');
+        $this->dir = $this->getData()['project_dir'];
+        $this->line(exec('clear'));
+        $this->logo();
+        $this->init();
+    }
+    
+    private function init()
+    {
+        //name of project
+        if (!empty($this->argument('name'))) {
+            $this->name = $this->argument('name');
+        } else {
+            //planing to generate random names from a new package.
+            $this->name = 'nette-blog';
+        }
+    
+        //set path
+        if (!empty($this->option('path'))) {
+            $this->path = $this->option('path');
+        } elseif (!empty($this->dir) && is_dir($this->dir)) {
+            $this->path = $this->dir;
+        } else {
+            $this->path = '/sdcard';
+        }
+        
+        //check if directory exists
+        if (!$this->checkDir()) {
+            exit();
+        } else {
+            $this->line(exec('tput sgr0'));
+            $this->info('Creating Nette app');
+            $this->newline();
+            $this->create();
+            $this->newline();
+            $this->comment("Nette App created successfully on {$this->path}/{$this->name}");
+        }
+    }
+    
+    private function create()
+    {
+        $cmd = "cd {$this->path} && composer create-project nette/web-project \"{$this->name}\"";
+        $this->exec($cmd);
+    }
+    
+    private function exec($command)
+    {
+        $this->line(exec($command));
+    }
+    
+    public function logo()
+    {
+        $figlet = new \Laminas\Text\Figlet\Figlet();
+        $this->comment($figlet->render("Nette"));
+    }
+    
+    private function checkDir()
+    {
+        if (file_exists($this->path . '/' . $this->name)) {
+            $this->error("A duplicate file/directory found in the path. Please choose a better name.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public function getData()
+    {
+        $json_object = file_get_contents(config('settings.PATH').'/settings.json');
+        $data = json_decode($json_object, true);
+        return $data;
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
