1a7be3a8d7fee9583f63b565e58a6705dd6a7659
Albin Varghese <albin8285@gmail.com>
2021-06-05 16:40:49 +0530

    create command for starting wordpress server

--- app/Commands/Server/Wordpress.php
+++ app/Commands/Server/Wordpress.php
@@ -12,14 +12,16 @@ class Wordpress extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'server:wordpress
+							{--n}
+							{--port=}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Start wordpress site';
 
     /**
      * Execute the console command.
@@ -28,7 +30,54 @@ class Wordpress extends Command
      */
     public function handle()
     {
-        //
+        $this->callSilently('settings:init');
+        $this->setPort();
+        $this->checkInstallations();
+    }
+    
+    public function checkInstallations()
+    {
+        echo shell_exec("clear");
+        $this->info("");
+        $this->logo();
+        $this->info("\n");
+        $dir = config('wordpress.PATH');
+        $cmd = "php -S 127.0.0.1:{$this->port} -t {$dir}";
+        $this->comment("Starting wordpress at : http://127.0.0.1:{$this->port}");
+        $this->info("");
+        $this->launch();
+        shell_exec($cmd);
+    }
+    
+    public function logo()
+    {
+        $figlet = new \Laminas\Text\Figlet\Figlet();
+        echo $figlet->setFont(config('logo.font'))->render(config('logo.name'));
+    }
+    
+    private function setPort()
+    {
+        if (!empty($this->option('port'))) {
+            $this->port = $this->option('port');
+        } elseif (!empty($this->getPort())) {
+            $this->port = $this->getPort();
+        } else {
+            $this->port = config('WORDPRESS_PORT');
+        }
+    }
+    
+    private function launch()
+    {
+        if (!$this->option('n')) {
+            return shell_exec("xdg-open http://127.0.0.1:{$this->port}");
+        }
+    }
+    
+    public function getPort()
+    {
+        $json_object = file_get_contents(config('settings.PATH').'/settings.json');
+        $data = json_decode($json_object, true);
+        return $data['wordpress_port'];
     }
 
     /**
