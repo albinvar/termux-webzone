85be38911bfde99a830dd472c1eb50b6009ba000
Albin Varghese <albin8285@gmail.com>
2021-07-15 23:28:53 +0530

    use helpers for manager.php

--- app/Commands/Manager.php
+++ app/Commands/Manager.php
@@ -6,13 +6,17 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Laminas\Text\Figlet\Figlet;
+use App\Helpers\Downloader;
+use Storage;
 use LaravelZero\Framework\Commands\Command;
 
 class Manager extends Command
 {
-    protected $fileName;
+    protected $fileName = 'index.php';
 
     protected $link;
+    
+    protected $dir = 'manager';
 
     protected $manager;
 
@@ -34,18 +38,39 @@ class Manager extends Command
     /**
      * Execute the console command.
      */
-    public function handle(): mixed
+    public function handle(): void
     {
-        $this->fileName = 'index.php';
-        $this->link = config('pma.MANAGER_DOWNLOAD_LINK');
-        $this->manager = config('pma.MANAGER_PATH');
-        pcntl_async_signals(true);
-        // Catch the cancellation of the action
+    	pcntl_async_signals(true);
+       
+        $this->link = config('manager.MANAGER_DOWNLOAD_LINK');
+        $this->manager = config('manager.MANAGER_PATH');
+        
         pcntl_signal(SIGINT, function (): void {
             $this->newline();
             $this->comment("\nShutting down...\n");
         });
+        
         $this->checkInstallation();
+    }
+    
+    private function start(): void
+    {
+        $this->line(exec('clear'));
+        $this->logo();
+        $this->info('Starting Webzone Manager....');
+        $this->newline();
+        $this->comment(exec("cd {$this->manager} && xdg-open http://127.0.0.1:9876/ && php -S 127.0.0.1:9876"));
+    }
+    
+    private function checkInstallation(): void
+    {
+        if (file_exists($this->manager) && file_exists($this->manager . '/' . $this->fileName) && ! $this->option('force')) {
+            $this->start();
+        } else {
+            $this->install();
+            sleep(5);
+            $this->start();
+        }
     }
 
     public function logo(): void
@@ -56,11 +81,40 @@ class Manager extends Command
 
     public function install(): void
     {
-        $link = config('pma.MANAGER_DOWNLOAD_LINK');
-        $lines = shell_exec("curl -w '\n%{http_code}\n' {$link} -o {$this->manager}/{$this->fileName} && chmod +x {$this->manager}/{$this->fileName}");
-        $lines = explode("\n", trim($lines));
-        $status = $lines[count($lines) - 1];
-        $this->checkDownloadStatus($status);
+    	$this->createDirectory();
+    
+    	$this->download();
+    
+	    $this->newline();
+	
+		$this->info('Installation Successful. Starting webzone manager for you...');
+    }
+    
+    private function createDirectory()
+    {
+        $this->task('Creating Required Folders ', function () {
+            try {
+            Storage::makeDirectory($this->dir);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+        });
+    }
+    
+    private function download()
+    {
+        $downloadTask = $this->task('Downloading resources ', function () {
+            $this->downloader = new Downloader($this->link, 'manager/index.php', 'local');
+            $response = $this->downloader->download();
+
+            if ($response['ok']) {
+                return true;
+            } else {
+                $this->error($response['error']->getMessage());
+                return false;
+            }
+        });
     }
 
     public function stop(): void
@@ -74,48 +128,5 @@ class Manager extends Command
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
-    }
-
-    private function checkInstallation(): void
-    {
-        if (file_exists($this->manager) && file_exists($this->manager . '/' . $this->fileName) && ! $this->option('force')) {
-            $this->start();
-        } else {
-            $this->task('Creating Required Folders', function (): void {
-                exec("mkdir -p {$this->manager}");
-            });
-
-            $this->task('Creating Required Files', function (): void {
-                $this->install();
-            });
-            $this->start();
-        }
-    }
-
-    private function start(): void
-    {
-        $this->line(exec('clear'));
-        $this->logo();
-        $this->info('Starting Temrux Manager....');
-        $this->newline();
-        $this->comment(exec("cd {$this->manager} && xdg-open http://127.0.0.1:9876/ && php -S 127.0.0.1:9876"));
-    }
-
-    private function checkDownloadStatus($status): void
-    {
-        switch ($status) {
-            case 000:
-                $this->error('Cannot connect to Server');
-                break;
-            case 200:
-                $this->comment("\nDownloaded Successfully...!!!");
-
-                break;
-            case 404:
-                $this->error('File not found on server..');
-                break;
-            default:
-                $this->error('An Unknown Error occurred...');
-        }
     }
 }
--- config/pma.php
+++ config/pma.php
@@ -157,26 +157,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | File Manager Download Link
-    |--------------------------------------------------------------------------
-    |
-    | Set file manager php Download link.
-    |
-    */
-     'MANAGER_DOWNLOAD_LINK' => 'https://gitlab.com/albinvar/pma-cli/-/raw/master/fm.php?inline=false',
-
-    /*
-    |--------------------------------------------------------------------------
-    | File Manager Download Link
-    |--------------------------------------------------------------------------
-    |
-    | Set file manager php Download link.
-    |
-    */
-     'MANAGER_PATH' => '/data/data/com.termux/files/usr/var/webzone/manager',
-
-    /*
-    |--------------------------------------------------------------------------
     | PHPSTAN Path
     |--------------------------------------------------------------------------
     |
