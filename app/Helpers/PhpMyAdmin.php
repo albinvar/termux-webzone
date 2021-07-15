688425bde71d216e68937659afe8bd8d1e0bb93d
Albin Varghese <albin8285@gmail.com>
2021-07-15 15:41:32 +0530

    add configurator method tp configure phpMyAdmin

--- app/Helpers/PhpMyAdmin.php
+++ app/Helpers/PhpMyAdmin.php
@@ -3,11 +3,13 @@
 namespace App\Helpers;
 
 use App\Helpers\Webzone;
+use Storage;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 
 class PhpMyAdmin extends Webzone
 {
-	public $apiUrl = 'https://www.phpmyadmin.net/home_page/version.json';
+	public $endpoint = 'https://www.phpmyadmin.net/home_page/version.json';
 	
 	public function __construct()
 	{
@@ -18,9 +20,33 @@ class PhpMyAdmin extends Webzone
 	public function latestRelease()
 	{
 		try {
-			return Http::acceptJson()->get($this->apiUrl)->json();
+			return Http::acceptJson()->get($this->endpoint)->json();
 		} catch (\Exception $e) {
 			return false;
+		}
+	}
+	
+	public function configurator(String $path, String $filename)
+	{
+		$blowfish = hash('tiger128,4', date("hisa"));
+		
+		$oldLines = [
+			'$cfg[\'Servers\'][$i][\'AllowNoPassword\'] = false;',
+			'$cfg[\'blowfish_secret\'] = \'\'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */',
+		];
+		
+		$newLines = [
+			'$cfg[\'Servers\'][$i][\'AllowNoPassword\'] = true;',
+			'$cfg[\'blowfish_secret\'] = \''.$blowfish.'\';',
+		];
+		
+		try{
+			$contents = Storage::get($path.'/'.$filename);
+			$str = str_replace($oldLines, $newLines, $contents);
+			Storage::disk('local')->put($path.'/config.sample.inc.php', $str);
+				return true;
+		} catch(\Exception $e) {
+				return false;
 		}
 	}
 }
