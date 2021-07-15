7c5fd60be69142c1842ac188d7af512747d6ca08
Albin Varghese <albin8285@gmail.com>
2021-07-15 15:46:04 +0530

    create directory if not exists for storing tmp files

--- app/Helpers/Downloader.php
+++ app/Helpers/Downloader.php
@@ -5,6 +5,7 @@ namespace App\Helpers;
 
 use App\Helpers\Webzone;
 use GuzzleHttp\Client;
+use Storage;
 use GuzzleHttp\Psr7\Utils;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Http;
@@ -18,7 +19,9 @@ class Downloader extends Webzone
 	
 	protected $dir;
 	
-	public function __construct(String $url, String $dir)
+	protected $saveTo;
+	
+	public function __construct(String $url, String $dir, String $saveTo='tmp')
 	{
 		parent::__construct();
 		
@@ -26,18 +29,33 @@ class Downloader extends Webzone
 		
 		$this->url = $url;
 		$this->dir = $dir;
+		
+		$this->saveTo = $saveTo;
 	}
 	
 	public function download()
     {
+    	if(!$this->createDirIfNotExists()) { return ['ok' => false, 'error' => 'Cannot create directory']; }
+	    
 	    $resource = Utils::tryFopen($this->dir, 'w');
 		$stream = Utils::streamFor($resource);
 		
 		try {
 			$res = $this->client->request('GET', $this->url, ['save_to' => $stream]);
-			return ['ok' => true, 'status_code' => $res->getStatusCode(), 'error' => null];
+			return ['ok' => true, 'status_code' => $res->getStatusCode(), 'error' => null, 'path' => $this->dir];
 		} catch (RequestException $e) {
 			return ['ok' => false, 'error' => $e];
+		}
+    }
+    
+    
+    private function createDirIfNotExists()
+    {
+    	  try {
+	        Storage::makeDirectory($this->saveTo);
+			return true;
+		} catch(\Exception $e) {
+			return false;
 		}
     }
 }
