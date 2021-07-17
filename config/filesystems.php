0205df3a201b03038639e9155358ae56e5912678
Albin Varghese <albin8285@gmail.com>
2021-07-17 11:39:11 +0530

    create new disk projects

--- config/filesystems.php
+++ config/filesystems.php
@@ -7,6 +7,10 @@ return [
             'driver' => 'local',
             'root' => "/data/data/com.termux/files/usr/var/webzone",
         ],
+        'projects' => [
+            'driver' => 'local',
+            'root' => "/sdcard/www",
+        ],
         'tmp' => [
             'driver' => 'local',
             'root' => "/data/data/com.termux/files/usr/var/webzone/tmp",
