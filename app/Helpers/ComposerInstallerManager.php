dc520289f73b5bb33d4809bf5f3e13cde7c62b7f
Albin Varghese <albin8285@gmail.com>
2021-08-24 20:49:03 +0530

    create seperate class of installtion of installers

---
+++ app/Helpers/ComposerInstallerManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Filesystem\FilesystemAdapter;
+use Storage;
+
+class ComposerInstallerManager
+{
+	
+	private static $_instance = null;
+	
+	private static $package;
+	
+	private static $cliName;
+	
+	private static $disk;
+	
+	public static function package($packageName, $cliName=null, $disk=null)
+    {
+    	self::$package = $packageName;
+	    self::$cliName = $cliName;
+		self::$disk = ($disk instanceof FilesystemAdapter)
+						? $disk
+						: null;
+						
+        self::$_instance = (self::$_instance === null) ? new self : null;
+
+        return self::$_instance;
+    }
+	
+	public function install()
+	{
+		exec('composer global require ' . static::$package);
+		
+		if(is_null(self::$disk))
+		{
+			return true;
+		}
+		
+		return $disk->has(self::$cliName);
+	}
+	
+	public function uninstall()
+	{
+		exec('composer global remove ' . static::$package);
+		
+		if(is_null(self::$disk))
+		{
+			return true;
+		}
+		
+		return ! self::$disk->has(self::$cliName);
+	}
+}
