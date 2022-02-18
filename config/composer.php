6d6cd20e05fed59bc309de8a2a4e640161d21102
Albin Varghese <albin8285@gmail.com>
2022-02-19 00:15:36 +0530

    fix composer:global initializer

---
+++ config/composer.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | PATH Environment Variable
+    |--------------------------------------------------------------------------
+    |
+    | used on bash.bashrc
+    |
+    */
+     'PATH_CONFIG' => 'export PATH="/data/data/com.termux/files/home/.composer/vendor/bin:$PATH"'
+
+
+];
